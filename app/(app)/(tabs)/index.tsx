7d9c7c790fb03c1f4bb9498c2658bf1af05d7c2c
Lucas Losekann <lucas1losekann@gmail.com>
2024-12-10 09:34:47 -0300

    configuração do Prettier, atualização de dependências e melhorias no layout

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,141 +1,168 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { StyleSheet, FlatList, TouchableOpacity, ImageBackground } from 'react-native'
-import { Text } from '../../../src/components/Text'
-import React, { useEffect, useState } from 'react'
-import * as Location from 'expo-location';
-import { useQuery } from '@tanstack/react-query';
-import { getLaundries, getRoute } from '../../../src/services/api';
-import { router } from 'expo-router';
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+	StyleSheet,
+	FlatList,
+	TouchableOpacity,
+	ImageBackground,
+	Alert,
+} from "react-native";
+import { Text } from "../../../src/components/Text";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { useQuery } from "@tanstack/react-query";
+import { getLaundries, getRoute } from "../../../src/services/api";
+import { router } from "expo-router";
 
 export default function Closest() {
-    const [location, setLocation] = useState<null | Location.LocationObject>(null);
-    const [errorMsg, setErrorMsg] = useState<null | string>(null);
-    const [laundriesData, setLaundriesData] = useState<null | any>(null);
+	const [location, setLocation] = useState<null | Location.LocationObject>(
+		null
+	);
+	const [errorMsg, setErrorMsg] = useState<null | string>(null);
+	const [laundriesData, setLaundriesData] = useState<null | any>(null);
 
-    const LaundriesQuery = useQuery({
-        queryKey: ['laundries'],
-        queryFn: async () => {
-            const response = await getLaundries()
-            return response.data;
-        },
-    })
+	const LaundriesQuery = useQuery({
+		queryKey: ["laundries"],
+		queryFn: async () => {
+			const response = await getLaundries();
+			return response.data;
+		},
+	});
 
-    useEffect(() => {
-        (async () => {
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
 
-            let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				Alert.alert(
+					"Permissão negada",
+					"Sem acesso à localização não será possível calcular a distância até as lavanderias mais próximas."
+				);
+				return;
+			}
 
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
+			let location = await Location.getCurrentPositionAsync({});
+			setLocation(location);
+		})();
+	}, []);
 
+	useEffect(() => {
+		if (LaundriesQuery.data) {
+			setLaundriesData(LaundriesQuery.data);
+			if (location) {
+				const laundries = LaundriesQuery.data;
+				async function calculateDistance(
+					location: Location.LocationObject
+				) {
+					await Promise.all(
+						laundries.map(async (laundry: any) => {
+							const distance = await getRoute({
+								latitude1: location.coords.latitude.toString(),
+								longitude1:
+									location.coords.longitude.toString(),
+								latitude2: laundry.endereco.latitude,
+								longitude2: laundry.endereco.longitude,
+							}).then((response) => {
+								return response.data.routes[0].distance / 1000;
+							});
+							setLaundriesData((prev: any) =>
+								prev.map((prevLaundry: any) => {
+									if (prevLaundry.id === laundry.id) {
+										return {
+											...prevLaundry,
+											distance,
+										};
+									}
+									return prevLaundry;
+								})
+							);
+						})
+					);
+				}
+				calculateDistance(location);
+			}
+		}
+	}, [LaundriesQuery.data, location]);
 
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        })();
-    }, []);
+	return (
+		<ImageBackground
+			source={require("../../../src/assets/images/bg_img.png")}
+			resizeMode="cover"
+			style={{
+				flex: 1,
+			}}
+		>
+			<SafeAreaView style={styles.container}>
+				<FlatList
+					data={laundriesData?.sort((a: any, b: any) => {
+						const distanceA = a.distance;
+						const distanceB = b.distance;
+						if (distanceA === undefined && distanceB === undefined)
+							return 0;
+						if (distanceA === undefined) return 1;
+						if (distanceB === undefined) return -1;
 
+						return distanceA - distanceB;
+					})}
+					renderItem={({ item }) => {
+						const distance = item.distance;
 
-    useEffect(() => {
-        if (LaundriesQuery.data) {
-            setLaundriesData(LaundriesQuery.data)
-            if (location) {
-                const laundries = LaundriesQuery.data
-                async function calculateDistance(location: Location.LocationObject) {
-                    await Promise.all(laundries.map(async (laundry: any) => {
-                        const distance = await getRoute({
-                            latitude1: location.coords.latitude.toString(),
-                            longitude1: location.coords.longitude.toString(),
-                            latitude2: laundry.endereco.latitude,
-                            longitude2: laundry.endereco.longitude
-                        }).then((response) => {
-                            return response.data.routes[0].distance / 1000
-                        })
-                        setLaundriesData((prev: any) => prev.map((prevLaundry: any) => {
-                            if (prevLaundry.id === laundry.id) {
-                                return {
-                                    ...prevLaundry,
-                                    distance
-                                }
-                            }
-                            return prevLaundry
-                        }))
-                    }))
-                }
-                calculateDistance(location)
-            }
-        }
-    }, [LaundriesQuery.data])
-
-
-
-    return (
-        <ImageBackground source={require("../../../src/assets/images/bg_img.png")} resizeMode="cover" style={{
-            flex: 1
-        }}>
-            <SafeAreaView style={styles.container}>
-                <FlatList
-                    data={laundriesData?.sort((a: any, b: any) => {
-
-                        const distanceA = a.distance
-                        const distanceB = b.distance
-                        if (distanceA === undefined && distanceB === undefined) return 0
-                        if (distanceA === undefined) return 1
-                        if (distanceB === undefined) return -1
-
-                        return distanceA - distanceB
-                    })}
-                    renderItem={({ item }) => {
-                        const distance = item.distance
-
-                        return (
-                            <TouchableOpacity style={styles.item}
-                                onPress={() => {
-                                    router.push({
-                                        pathname: '/(app)/laundry',
-                                        params: {
-                                            id: item.id
-                                        }
-                                    })
-                                }}>
-                                <Text style={styles.title} className='font-ms600 text-[#093a3f]'>{item.nome}</Text>
-                                <Text className='font-ms400 text-sm mb-2 text-[#093a3f]'>{item.endereco.cidade.nome}</Text>
-                                {
-                                    distance ? <Text className='font-ms600 text-xs text-[#093a3f]'>Distância: {distance.toFixed(2)} km</Text> : null
-                                }
-                            </TouchableOpacity>
-                        )
-                    }}
-                    keyExtractor={item => item.id}
-                />
-            </SafeAreaView>
-        </ImageBackground>
-    );
+						return (
+							<TouchableOpacity
+								style={styles.item}
+								onPress={() => {
+									router.push({
+										pathname: "/(app)/laundry",
+										params: {
+											id: item.id,
+										},
+									});
+								}}
+							>
+								<Text
+									style={styles.title}
+									className="font-ms600 text-[#093a3f]"
+								>
+									{item.nome}
+								</Text>
+								<Text className="font-ms400 text-sm mb-2 text-[#093a3f]">
+									{item.endereco.cidade.nome}
+								</Text>
+								{distance ? (
+									<Text className="font-ms600 text-xs text-[#093a3f]">
+										Distância: {distance.toFixed(2)} km
+									</Text>
+								) : null}
+							</TouchableOpacity>
+						);
+					}}
+					keyExtractor={(item) => item.id}
+				/>
+			</SafeAreaView>
+		</ImageBackground>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 20,
-    },
-    paragraph: {
-        fontSize: 18,
-        textAlign: 'center',
-    },
-    item: {
-        backgroundColor: '#dddddd',
-        padding: 20,
-        marginVertical: 8,
-        borderRadius: 16,
-        borderColor: "#093a3f",
-        borderWidth: 1
-    },
-    title: {
-        fontSize: 16,
-        textTransform: "uppercase"
-    },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		paddingVertical: 20,
+	},
+	paragraph: {
+		fontSize: 18,
+		textAlign: "center",
+	},
+	item: {
+		backgroundColor: "#dddddd",
+		padding: 20,
+		marginVertical: 8,
+		borderRadius: 16,
+		borderColor: "#093a3f",
+		borderWidth: 1,
+	},
+	title: {
+		fontSize: 16,
+		textTransform: "uppercase",
+	},
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,83 +1,85 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import "../src/assets/styles/global.css";
-import 'react-native-reanimated';
-import Toast from 'react-native-toast-message';
+import "react-native-reanimated";
+import Toast from "react-native-toast-message";
 
 import {
-  Montserrat_100Thin,
-  Montserrat_200ExtraLight,
-  Montserrat_300Light,
-  Montserrat_400Regular,
-  Montserrat_500Medium,
-  Montserrat_600SemiBold,
-  Montserrat_700Bold,
-  Montserrat_800ExtraBold,
-  Montserrat_900Black,
-  Montserrat_100Thin_Italic,
-  Montserrat_200ExtraLight_Italic,
-  Montserrat_300Light_Italic,
-  Montserrat_400Regular_Italic,
-  Montserrat_500Medium_Italic,
-  Montserrat_600SemiBold_Italic,
-  Montserrat_700Bold_Italic,
-  Montserrat_800ExtraBold_Italic,
-  Montserrat_900Black_Italic,
-} from '@expo-google-fonts/montserrat';
+	Montserrat_100Thin,
+	Montserrat_200ExtraLight,
+	Montserrat_300Light,
+	Montserrat_400Regular,
+	Montserrat_500Medium,
+	Montserrat_600SemiBold,
+	Montserrat_700Bold,
+	Montserrat_800ExtraBold,
+	Montserrat_900Black,
+	Montserrat_100Thin_Italic,
+	Montserrat_200ExtraLight_Italic,
+	Montserrat_300Light_Italic,
+	Montserrat_400Regular_Italic,
+	Montserrat_500Medium_Italic,
+	Montserrat_600SemiBold_Italic,
+	Montserrat_700Bold_Italic,
+	Montserrat_800ExtraBold_Italic,
+	Montserrat_900Black_Italic,
+} from "@expo-google-fonts/montserrat";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from '../src/contexts/AuthContext';
-import { StatusBar } from 'expo-status-bar';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "../src/contexts/AuthContext";
+import { StatusBar } from "expo-status-bar";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  let [loaded] = useFonts({
-    Montserrat_100Thin,
-    Montserrat_200ExtraLight,
-    Montserrat_300Light,
-    Montserrat_400Regular,
-    Montserrat_500Medium,
-    Montserrat_600SemiBold,
-    Montserrat_700Bold,
-    Montserrat_800ExtraBold,
-    Montserrat_900Black,
-    Montserrat_100Thin_Italic,
-    Montserrat_200ExtraLight_Italic,
-    Montserrat_300Light_Italic,
-    Montserrat_400Regular_Italic,
-    Montserrat_500Medium_Italic,
-    Montserrat_600SemiBold_Italic,
-    Montserrat_700Bold_Italic,
-    Montserrat_800ExtraBold_Italic,
-    Montserrat_900Black_Italic,
-  });
+	let [loaded] = useFonts({
+		Montserrat_100Thin,
+		Montserrat_200ExtraLight,
+		Montserrat_300Light,
+		Montserrat_400Regular,
+		Montserrat_500Medium,
+		Montserrat_600SemiBold,
+		Montserrat_700Bold,
+		Montserrat_800ExtraBold,
+		Montserrat_900Black,
+		Montserrat_100Thin_Italic,
+		Montserrat_200ExtraLight_Italic,
+		Montserrat_300Light_Italic,
+		Montserrat_400Regular_Italic,
+		Montserrat_500Medium_Italic,
+		Montserrat_600SemiBold_Italic,
+		Montserrat_700Bold_Italic,
+		Montserrat_800ExtraBold_Italic,
+		Montserrat_900Black_Italic,
+	});
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Stack screenOptions={{
-            headerShown: false
-          }} />
-          <StatusBar style="inverted" />
-        </AuthProvider>
-      </QueryClientProvider>
-      <Toast />
-    </>
-  );
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<AuthProvider>
+					<Stack
+						screenOptions={{
+							headerShown: false,
+						}}
+					/>
+					<StatusBar style="inverted" />
+				</AuthProvider>
+			</QueryClientProvider>
+			<Toast />
+		</>
+	);
 }
